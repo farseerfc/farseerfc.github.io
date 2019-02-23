64b89f41d6cb662b7a98107672cc40a1f553cb9a
farseerfc <farseerfc@gmail.com>
2019-02-23 23:44:38 +0000

    update from travis

---
+++ theme/js/other_blog.js
@@ -0,0 +1,29 @@
+var other_blog = (function(){
+  function escapeHtml(str) {
+    return $('<div/>').text(str).html();
+  }
+  function render(data, t){
+    var target = $(data).find('h2');
+    var i = 0;
+    var fragment = '<div class="list-group" id="other_blog">';
+    for(i = 0; i < target.length; i++ ){
+        fragment += '<div class="list-group-item">'
+        fragment += '<a class="btn btn-default withripple" href="' + target[i].parentNode.href + '" target="_blank">'+target[i].innerText+'</a>'
+        fragment += '</div>'
+    }
+    fragment += '</div>';
+    $(t).html(fragment);
+  }
+  return {
+    showRepos: function(options){
+      $.ajax({
+          url: options.url
+        , dataType: 'html'
+        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
+        , success: function(data) {
+          render(data, options.target);
+        }
+      });
+    }
+  };
+})();
